a749ad9239392a4af658f0caca27c323d237d426
AlexKMarshall <alexmarshall@qogita.com>
2024-09-16 11:32:23 +0100

    gp

--- vite.config.ts
+++ vite.config.ts
@@ -2,15 +2,18 @@ import { vitePlugin as remix } from '@remix-run/dev'
 import { defineConfig } from 'vite'
 import tsconfigPaths from 'vite-tsconfig-paths'
 
+const isStorybook = process.argv[1]?.includes('storybook')
+
 export default defineConfig({
 	plugins: [
-		remix({
-			future: {
-				v3_fetcherPersist: true,
-				v3_relativeSplatPath: true,
-				v3_throwAbortReason: true,
-			},
-		}),
+		!isStorybook &&
+			remix({
+				future: {
+					v3_fetcherPersist: true,
+					v3_relativeSplatPath: true,
+					v3_throwAbortReason: true,
+				},
+			}),
 		tsconfigPaths(),
 	],
 })
