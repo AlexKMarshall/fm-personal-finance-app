ff6330acd1f576c0d88a8efb715938fac05cd991
Alex Marshall <alexmarshall@qogita.com>
2024-09-16 09:47:41 +0100

    Alexkmarshall83/ale 102 handle email already exists (#18)

---
+++ tests/factories/user.ts
@@ -0,0 +1,18 @@
+import { ObjectFactory } from './utils'
+import { faker } from '@faker-js/faker'
+
+export const makeUser: ObjectFactory<{
+	name: string
+	email: string
+	password: string
+}> = (overrides) => {
+	const name = overrides?.name ?? faker.person.fullName()
+	const email = overrides?.email ?? faker.internet.email()
+	const password = overrides?.password ?? faker.internet.password()
+
+	return {
+		name,
+		email,
+		password,
+	}
+}
