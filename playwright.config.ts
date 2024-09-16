300ef6d171fb5a4244fc0ae24ed5550745fd0231
AlexKMarshall <alexmarshall@qogita.com>
2024-09-16 07:46:31 +0100

    ci: add correct port to tests

--- playwright.config.ts
+++ playwright.config.ts
@@ -7,8 +7,7 @@ import { defineConfig, devices } from '@playwright/test'
 // import dotenv from 'dotenv';
 // dotenv.config({ path: path.resolve(__dirname, '.env') });
 
-// biome-ignore lint/suspicious/noConsole: temporary debugging
-console.log('process.env.CI', process.env.CI)
+const PORT = process.env.CI ? 3000 : 5173
 
 /**
  * See https://playwright.dev/docs/test-configuration.
@@ -28,7 +27,7 @@ export default defineConfig({
 	/* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
 	use: {
 		/* Base URL to use in actions like `await page.goto('/')`. */
-		baseURL: 'http://localhost:5173',
+		baseURL: `http://localhost:${PORT}`,
 
 		/* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
 		trace: 'on-first-retry',
