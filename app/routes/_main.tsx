5989f9c06f00c27d63f3c648c36739d5333af56f
AlexKMarshall <alexmarshall@qogita.com>
2024-09-18 16:52:07 +0100

    feat: use site navigation on main pages

--- app/routes/_main.tsx
+++ app/routes/_main.tsx
@@ -1,7 +1,7 @@
 import type { LoaderFunctionArgs } from '@remix-run/node'
-import { Link, Outlet, useLoaderData } from '@remix-run/react'
+import { Outlet, useLoaderData } from '@remix-run/react'
 import { requireAuthCookie } from '~/auth.server'
-import { Icon } from '~/components/Icon'
+import { Navigation, NavigationItem } from '~/components/Navigation'
 
 export async function loader({ request }: LoaderFunctionArgs) {
 	const { name } = await requireAuthCookie(request)
@@ -15,28 +15,23 @@ export default function Main() {
 		<div>
 			<Outlet />
 			<p>{name}</p>
-			<nav>
-				<Link to="/overview">
-					<Icon name="Overview" className="size-6 text-green" />
-					<span className="sr-only">Overview</span>
-				</Link>
-				<Link to="/transactions">
-					<Icon name="Transactions" className="size-6" />
-					<span className="sr-only">Transactions</span>
-				</Link>
-				<Link to="/budgets">
-					<Icon name="Budgets" className="size-6" />
-					<span className="sr-only">Budgets</span>
-				</Link>
-				<Link to="/pots">
-					<Icon name="Pots" className="size-6" />
-					<span className="sr-only">Pots</span>
-				</Link>
-				<Link to="/recurring-bills">
-					<Icon name="RecurringBills" className="size-6" />
-					<span className="sr-only">Recurring Bills</span>
-				</Link>
-			</nav>
+			<Navigation>
+				<NavigationItem to="/overview" icon="Overview">
+					Overview
+				</NavigationItem>
+				<NavigationItem to="/transactions" icon="Transactions">
+					Transactions
+				</NavigationItem>
+				<NavigationItem to="/budgets" icon="Budgets">
+					Budgets
+				</NavigationItem>
+				<NavigationItem to="/pots" icon="Pots">
+					Pots
+				</NavigationItem>
+				<NavigationItem to="/recurring-bills" icon="RecurringBills">
+					Recurring Bills
+				</NavigationItem>
+			</Navigation>
 		</div>
 	)
 }
