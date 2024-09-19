e413dd47906bdc18d543ffc8e2ff47cb54ceeb59
AlexKMarshall <alexmarshall@qogita.com>
2024-09-19 14:38:34 +0100

    style: responsive transactions table

--- app/routes/_main.transactions/Transaction.tsx
+++ app/routes/_main.transactions/Transaction.tsx
@@ -1,5 +1,6 @@
 import clsx from 'clsx'
 import { type ComponentProps } from 'react'
+import { tv } from 'tailwind-variants'
 
 export function Transactions({
 	transactions,
@@ -7,7 +8,91 @@ export function Transactions({
 	transactions: Array<ComponentProps<typeof Transaction> & { id: string }>
 }) {
 	return (
-		<ul>
+		<div>
+			<TransactionsTable
+				transactions={transactions}
+				className="hidden sm:table"
+			/>
+			<TransactionsList transactions={transactions} className="sm:hidden" />
+		</div>
+	)
+}
+
+const transactionsTableStyles = tv({
+	base: 'w-full table-fixed',
+})
+
+function TransactionsTable({
+	className,
+	transactions,
+}: {
+	className?: string
+	transactions: Array<ComponentProps<typeof Transaction> & { id: string }>
+}) {
+	return (
+		<table className={transactionsTableStyles({ className })}>
+			<thead>
+				<tr className="border-b border-gray-100">
+					<th className="w-1/2 py-3 pl-4 text-left text-xs font-normal text-gray-500">
+						Recipient / Sender
+					</th>
+					<th className="text-left text-xs font-normal text-gray-500">
+						Category
+					</th>
+					<th className="text-left text-xs font-normal text-gray-500">
+						Transaction Date
+					</th>
+					<th className="pr-4 text-right text-xs font-normal text-gray-500">
+						Amount
+					</th>
+				</tr>
+			</thead>
+			<tbody>
+				{transactions.map((transaction) => {
+					const direction = transaction.amount.startsWith('-')
+						? 'debit'
+						: 'credit'
+					return (
+						<tr
+							key={transaction.id}
+							className="border-b border-gray-100 last:border-b-0"
+						>
+							<td className="py-4 pl-4 first:pt-6 last:pb-0">
+								<div className="flex items-center gap-4 text-sm font-bold">
+									<img
+										src={transaction.avatar}
+										alt=""
+										className="size-10 rounded-full"
+									/>
+									{transaction.name}
+								</div>
+							</td>
+							<td className="text-xs text-gray-500">{transaction.category}</td>
+							<td className="text-xs text-gray-500">{transaction.date}</td>
+							<td
+								className={clsx('pr-4 text-right text-sm font-bold', {
+									'text-green': direction === 'credit',
+								})}
+							>
+								{transaction.amount}
+							</td>
+						</tr>
+					)
+				})}
+			</tbody>
+		</table>
+	)
+}
+
+export function TransactionsList({
+	className,
+	transactions,
+}: {
+	className?: string
+	transactions: Array<ComponentProps<typeof Transaction> & { id: string }>
+}) {
+	return (
+		<ul className={className}>
 			{transactions.map((transaction) => (
 				<li
 					className="mb-4 border-b border-gray-100 pb-4 last:mb-0 last:border-b-0 last:pb-0"
@@ -39,7 +124,7 @@ export function Transaction({
 			<img
 				src={avatar}
 				alt=""
-				className="size-8 self-center rounded-full [grid-area:avatar]"
+				className="size-8 shrink-0 self-center rounded-full [grid-area:avatar]"
 			/>
 			<p className="text-sm font-bold leading-normal [grid-area:name]">
 				{name}
