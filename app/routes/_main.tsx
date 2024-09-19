3418bd069ea00431e4af0ee942478246b40e664a
AlexKMarshall <alexmarshall@qogita.com>
2024-09-19 10:53:21 +0100

    feat: remove name from main page content

--- app/routes/_main.tsx
+++ app/routes/_main.tsx
@@ -1,25 +1,21 @@
 import type { LoaderFunctionArgs } from '@remix-run/node'
-import { Outlet, useLoaderData } from '@remix-run/react'
+import { Outlet } from '@remix-run/react'
 import { requireAuthCookie } from '~/auth.server'
 import { Navigation, NavigationItem } from '~/components/Navigation'
 import { Sidebar } from '~/components/Sidebar'
 
 export async function loader({ request }: LoaderFunctionArgs) {
-	const { name } = await requireAuthCookie(request)
-
-	return { name }
+	await requireAuthCookie(request)
 }
 
 export default function Main() {
-	const { name } = useLoaderData<typeof loader>()
 	return (
 		<div className="grid min-h-svh grid-cols-[auto_1fr] grid-rows-[1fr_auto] flex-col [grid-template-areas:'sidebar-desktop_main'_'empty_sidebar-mobile']">
 			<Sidebar className="hidden [grid-area:sidebar-desktop] lg:flex">
 				<SiteNavigation />
 			</Sidebar>
-			<div className="flex-grow [grid-area:main]">
+			<div className="flex-grow px-4 py-6 [grid-area:main]">
 				<Outlet />
-				<p>{name}</p>
 			</div>
 			<Sidebar className="mt-auto [grid-area:sidebar-mobile] lg:hidden">
 				<SiteNavigation />
