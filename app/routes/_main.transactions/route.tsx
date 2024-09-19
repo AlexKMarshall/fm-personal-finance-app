930e1aaaf724fcc0296bf14b66f79d7804367231
AlexKMarshall <alexmarshall@qogita.com>
2024-09-19 21:05:24 +0100

    add category transaction filter

--- app/routes/_main.transactions/route.tsx
+++ app/routes/_main.transactions/route.tsx
@@ -1,4 +1,4 @@
-import { useLoaderData } from '@remix-run/react'
+import { Form, useLoaderData } from '@remix-run/react'
 
 import { Transactions } from './Transactions'
 import { Card } from '~/components/Card'
@@ -6,53 +6,144 @@ import { prisma } from '~/db/prisma.server'
 import type { LoaderFunctionArgs } from '@remix-run/node'
 import { requireAuthCookie } from '~/auth.server'
 import { formatCurrency, formatDate } from '~/utils/format'
+import { Label } from '~/components/Label'
+import { z } from 'zod'
+import { getFormProps, getSelectProps, useForm } from '@conform-to/react'
+import { getZodConstraint, parseWithZod } from '@conform-to/zod'
 
 export async function loader({ request }: LoaderFunctionArgs) {
 	const { userId } = await requireAuthCookie(request)
-	const transactions = await getTransactions({ userId })
 
-	return transactions.map(({ id, Counterparty, Category, amount, date }) => ({
-		id,
-		name: Counterparty.name,
-		category: Category.name,
-		date: formatDate(date),
-		amount: formatCurrency(amount),
-		avatar: Counterparty.avatarUrl,
-	}))
+	const searchParams = new URL(request.url).searchParams
+
+	const submission = parseWithZod(searchParams, { schema: filterSchema })
+	if (submission.status !== 'success') {
+		throw new Error('Invalid search params')
+	}
+
+	const [transactions, categories] = await Promise.all([
+		getTransactions({ userId, category: submission.value.category }),
+		getCategories({ userId }),
+	])
+
+	const formattedTransactions = transactions.map(
+		({ id, Counterparty, Category, amount, date }) => ({
+			id,
+			name: Counterparty.name,
+			category: Category.name,
+			date: formatDate(date),
+			amount: formatCurrency(amount),
+			avatar: Counterparty.avatarUrl,
+		}),
+	)
+
+	return {
+		transactions: formattedTransactions,
+		categories,
+	}
 }
 
+const filterSchema = z.object({
+	category: z.string().optional(),
+})
+
 export default function TransactionsRoute() {
-	const transactions = useLoaderData<typeof loader>()
+	const { transactions, categories } = useLoaderData<typeof loader>()
+
+	const [form, fields] = useForm({
+		constraint: getZodConstraint(filterSchema),
+		onValidate: ({ formData }) =>
+			parseWithZod(formData, { schema: filterSchema }),
+	})
+
 	return (
 		<>
 			<h1 className="text-3xl font-bold leading-relaxed">Transactions</h1>
 			<Card>
+				<Form
+					{...getFormProps(form)}
+					replace
+					onChange={(event) => {
+						event.currentTarget.requestSubmit()
+					}}
+				>
+					<div className="flex gap-3">
+						<Label className="sr-only sm:not-sr-only" htmlFor="category">
+							Category
+						</Label>
+						<select {...getSelectProps(fields.category)} id="category">
+							<option value="">All Transactions</option>
+							{categories.map((category) => (
+								<option
+									key={category.id}
+									value={category.name.toLocaleLowerCase()}
+								>
+									{category.name}
+								</option>
+							))}
+						</select>
+					</div>
+					<button type="submit">Filter</button>
+				</Form>
 				<Transactions transactions={transactions} />
 			</Card>
 		</>
 	)
 }
 
-function getTransactions({ userId }: { userId: string }) {
-	return prisma.transaction.findMany({
+function getTransactions({
+	userId,
+	category,
+}: {
+	userId: string
+	category?: string
+}) {
+	return prisma.transaction
+		.findMany({
+			where: {
+				userId,
+			},
+			select: {
+				id: true,
+				Counterparty: {
+					select: {
+						name: true,
+						avatarUrl: true,
+					},
+				},
+				amount: true,
+				date: true,
+				Category: {
+					select: {
+						name: true,
+					},
+				},
+			},
+		})
+		.then(
+			// Filter in memory as SQLite/Prisma doesn't support case-insensitive filtering
+			// The data sets should be small enough for this to be acceptably fast
+			(transactions) =>
+				transactions.filter((transaction) =>
+					category
+						? transaction.Category.name.toLocaleLowerCase() ===
+							category.toLocaleLowerCase()
+						: true,
+				),
+		)
+}
+
+function getCategories({ userId }: { userId: string }) {
+	return prisma.category.findMany({
 		where: {
-			userId,
+			Transactions: {
+				some: {
+					userId,
+				},
+			},
 		},
-		select: {
-			id: true,
-			Counterparty: {
-				select: {
-					name: true,
-					avatarUrl: true,
-				},
-			},
-			amount: true,
-			date: true,
-			Category: {
-				select: {
-					name: true,
-				},
-			},
+		orderBy: {
+			name: 'asc',
 		},
 	})
 }
--- tests/transactions.spec.ts
+++ tests/transactions.spec.ts
@@ -1,5 +1,5 @@
 import type { Page } from '@playwright/test'
-import { makeTransaction } from './factories/transaction'
+import { makeCategory, makeTransaction } from './factories/transaction'
 import { test, expect } from './playwright-utils.js'
 import { formatCurrency } from '~/utils/format'
 
@@ -19,6 +19,89 @@ test('shows a list of transactions', async ({
 	await transactionsPage.goto()
 
 	for (const transaction of transactions) {
+		await expect(
+			transactionsPage.transaction({
+				name: transaction.Counterparty.name,
+				amount: transaction.amount,
+			}),
+		).toBeVisible()
+	}
+})
+
+test('filter by category', async ({ page, signUp, login, seedDatabase }) => {
+	const user = await signUp()
+	const categoryOne = makeCategory()
+	const categoryTwo = makeCategory()
+	const categoryThree = makeCategory()
+	const transactionOne = makeTransaction({ Category: categoryOne })
+	const transactionTwo = makeTransaction({ Category: categoryTwo })
+	const transactionThree = makeTransaction({ Category: categoryThree })
+	await seedDatabase({
+		user,
+		transactions: [transactionOne, transactionTwo, transactionThree],
+	})
+	await login(user)
+	const transactionsPage = new TransactionsPage(page)
+
+	await transactionsPage.goto()
+
+	// By default nothing is filtered
+	for (const transaction of [
+		transactionOne,
+		transactionTwo,
+		transactionThree,
+	]) {
+		await expect(
+			transactionsPage.transaction({
+				name: transaction.Counterparty.name,
+				amount: transaction.amount,
+			}),
+		).toBeVisible()
+	}
+
+	// Filter by category one
+	await transactionsPage.filterByCategory(categoryOne.name)
+	// Only transaction one should be visible
+	await expect(
+		transactionsPage.transaction({
+			name: transactionOne.Counterparty.name,
+			amount: transactionOne.amount,
+		}),
+	).toBeVisible()
+	for (const transaction of [transactionTwo, transactionThree]) {
+		await expect(
+			transactionsPage.transaction({
+				name: transaction.Counterparty.name,
+				amount: transaction.amount,
+			}),
+		).not.toBeVisible()
+	}
+
+	// Filter by category two
+	await transactionsPage.filterByCategory(categoryTwo.name)
+	// Only transaction two should be visible
+	await expect(
+		transactionsPage.transaction({
+			name: transactionTwo.Counterparty.name,
+			amount: transactionTwo.amount,
+		}),
+	).toBeVisible()
+	for (const transaction of [transactionOne, transactionThree]) {
+		await expect(
+			transactionsPage.transaction({
+				name: transaction.Counterparty.name,
+				amount: transaction.amount,
+			}),
+		).not.toBeVisible()
+	}
+
+	// Select all transactions
+	await transactionsPage.filterByCategory('All Transactions')
+	for (const transaction of [
+		transactionOne,
+		transactionTwo,
+		transactionThree,
+	]) {
 		await expect(
 			transactionsPage.transaction({
 				name: transaction.Counterparty.name,
@@ -56,5 +139,11 @@ class TransactionsPage {
 			.getByRole('row')
 			.filter({ hasText: name })
 			.filter({ hasText: formattedAmount })
+	}
+
+	filterByCategory(category: string) {
+		return this.page.getByRole('combobox', { name: /category/i }).selectOption({
+			label: category,
+		})
 	}
 }
