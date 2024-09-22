7724f1b28503a5c4a6b43bcc8889f5b6101c3231
Alex Marshall <alexmarshall@qogita.com>
2024-09-22 13:48:46 +0100

    fix: auth image aspect ratio (#65)

--- app/routes/_auth.tsx
+++ app/routes/_auth.tsx
@@ -14,20 +14,27 @@ export async function loader({ request }: LoaderFunctionArgs) {
 
 export default function Auth() {
 	return (
-		<div className="flex min-h-svh flex-col lg:flex-row">
+		<div className="flex min-h-svh flex-col lg:grid lg:grid-cols-[auto_1fr]">
 			<div className="grid place-items-center rounded-b-lg bg-gray-900 p-6 lg:hidden lg:rounded-xl lg:p-0">
 				<img src="logo-large.svg" alt="finance" className="lg:hidden" />
 			</div>
-			<div className="hidden flex-1 p-5 [grid-template-areas:'stack'] *:[grid-area:stack] lg:grid">
-				<div className="flex flex-col items-start rounded-xl bg-gray-900 bg-[url('/illustration-authentication.svg')] bg-cover bg-no-repeat p-10">
-					<img src="logo-large.svg" alt="finance" />
-					<h2 className="mb-6 mt-auto text-3xl font-bold leading-tight text-white">
-						Keep track of your money and save for your future
-					</h2>
-					<p className="text-sm leading-normal text-white">
-						Personal finance app puts you in control of your spending. Track
-						transactions, set budgets, and add to savings pots easily.
-					</p>
+			<div className="hidden max-h-svh p-5 lg:grid">
+				<div className="grid aspect-[91/150] h-full overflow-hidden rounded-xl bg-gray-900 [grid-template-areas:'stack'] *:[grid-area:stack]">
+					<img
+						src="illustration-authentication.svg"
+						alt=""
+						className="h-full scale-[1.01]"
+					/>
+					<div className="relative flex h-full flex-col items-start p-10">
+						<img src="logo-large.svg" alt="finance" />
+						<h2 className="mb-6 mt-auto text-3xl font-bold leading-tight text-white">
+							Keep track of your money and save for your future
+						</h2>
+						<p className="text-sm leading-normal text-white">
+							Personal finance app puts you in control of your spending. Track
+							transactions, set budgets, and add to savings pots easily.
+						</p>
+					</div>
 				</div>
 			</div>
 			<div className="flex flex-1 items-center justify-center p-4 sm:p-6">
