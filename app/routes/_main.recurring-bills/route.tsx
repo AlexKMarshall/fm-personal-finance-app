f62dc52bbb817793747d41191e511e0ad1fc8646
AlexKMarshall <alexmarshall@qogita.com>
2024-09-23 10:27:43 +0100

    style: page layout

--- app/routes/_main.recurring-bills/route.tsx
+++ app/routes/_main.recurring-bills/route.tsx
@@ -69,58 +69,65 @@ export default function Overview() {
 	const { recurringBills, totalBills, paidBills, upcomingBills, soonBills } =
 		useLoaderData<typeof loader>()
 	return (
-		<>
+		<div className="flex flex-col gap-6 @container">
 			<h1 className="text-3xl font-bold leading-tight">Recurring Bills</h1>
-			<Card
-				theme="dark"
-				className="flex items-center gap-5 sm:flex-col sm:items-start sm:justify-between sm:gap-8"
-			>
-				<Icon name="RecurringBillsOutline" className="size-10" />
-				<div className="flex flex-col gap-3" data-testid="total-bills">
-					<h2 id="total-bills" className="text-sm leading-normal">
-						Total Bills
-					</h2>
-					<p className="text-3xl font-bold leading-tight">{totalBills}</p>
+			<div className="flex flex-col gap-6 @5xl:flex-row @5xl:items-start">
+				<div className="flex flex-col gap-3 @[40rem]:flex-row @[40rem]:gap-6 @5xl:flex-col">
+					<Card
+						theme="dark"
+						className="flex flex-1 items-center gap-5 @[40rem]:flex-col @[40rem]:items-start @[40rem]:justify-between @[40rem]:gap-8"
+					>
+						<Icon name="RecurringBillsOutline" className="size-10" />
+						<div className="flex flex-col gap-3" data-testid="total-bills">
+							<h2 id="total-bills" className="text-sm leading-normal">
+								Total Bills
+							</h2>
+							<p className="text-3xl font-bold leading-tight">{totalBills}</p>
+						</div>
+					</Card>
+					<Card theme="light" className="flex-1">
+						<h2 className="mb-5 font-bold">Summary</h2>
+						<dl>
+							<SummaryItem className="border-b border-gray-100 py-4 first:pt-0 last:border-b-0 last:pb-0">
+								<SummaryItem.Title>Total Paid</SummaryItem.Title>
+								<SummaryItem.Amount
+									count={paidBills.count}
+									total={paidBills.total}
+								/>
+							</SummaryItem>
+							<SummaryItem className="border-b border-gray-100 py-4 first:pt-0 last:border-b-0 last:pb-0">
+								<SummaryItem.Title>Total Upcoming</SummaryItem.Title>
+								<SummaryItem.Amount
+									count={upcomingBills.count}
+									total={upcomingBills.total}
+								/>
+							</SummaryItem>
+							<SummaryItem
+								color="red"
+								className="border-b border-gray-100 py-4 first:pt-0 last:border-b-0 last:pb-0"
+							>
+								<SummaryItem.Title>Due Soon</SummaryItem.Title>
+								<SummaryItem.Amount
+									count={soonBills.count}
+									total={soonBills.total}
+								/>
+							</SummaryItem>
+						</dl>
+					</Card>
 				</div>
-			</Card>
-			<Card theme="light">
-				<h2 className="font-bold">Summary</h2>
-				<dl>
-					<SummaryItem>
-						<SummaryItem.Title>Total Paid</SummaryItem.Title>
-						<SummaryItem.Amount
-							count={paidBills.count}
-							total={paidBills.total}
-						/>
-					</SummaryItem>
-					<SummaryItem>
-						<SummaryItem.Title>Total Upcoming</SummaryItem.Title>
-						<SummaryItem.Amount
-							count={upcomingBills.count}
-							total={upcomingBills.total}
-						/>
-					</SummaryItem>
-					<SummaryItem color="red">
-						<SummaryItem.Title>Due Soon</SummaryItem.Title>
-						<SummaryItem.Amount
-							count={soonBills.count}
-							total={soonBills.total}
-						/>
-					</SummaryItem>
-				</dl>
-			</Card>
-			<Card theme="light">
-				<RecurringBills recurringBills={recurringBills} />
-			</Card>
-		</>
+				<Card theme="light">
+					<RecurringBills recurringBills={recurringBills} />
+				</Card>
+			</div>
+		</div>
 	)
 }
 
 const summaryItemStyles = tv({
 	slots: {
-		base: 'flex justify-between',
-		title: 'text-xs',
-		amount: 'text-xs font-bold',
+		base: 'flex justify-between gap-16',
+		title: 'whitespace-nowrap text-xs',
+		amount: 'whitespace-nowrap text-xs font-bold',
 	},
 	variants: {
 		color: {
