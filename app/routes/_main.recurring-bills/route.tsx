170120a5a0aaa55475b1fd62f22fc61738c78b9a
Alex Marshall <alexmarshall@qogita.com>
2024-09-23 11:04:36 +0100

    Alexkmarshall83/ale 155 add bills summary (#73)

--- app/routes/_main.recurring-bills/route.tsx
+++ app/routes/_main.recurring-bills/route.tsx
@@ -7,6 +7,8 @@ import { formatCurrency, formatDayOfMonth } from '~/utils/format'
 import { RecurringBills } from './RecurringBills'
 import { addDays, addMonths, isBefore, startOfMonth, subMonths } from 'date-fns'
 import { Icon } from '~/components/Icon'
+import { tv } from 'tailwind-variants'
+import { useContext, createContext, type ReactNode } from 'react'
 
 export async function loader({ request }: LoaderFunctionArgs) {
 	const { userId } = await requireAuthCookie(request)
@@ -29,33 +31,167 @@ export async function loader({ request }: LoaderFunctionArgs) {
 	const totalBills = formatCurrency(
 		recurringBills.reduce((total, { amount }) => total + Math.abs(amount), 0),
 	)
+	const paidBills = {
+		count: recurringBills.filter((bill) => bill.status === 'paid').length,
+		total: formatCurrency(
+			recurringBills
+				.filter((bill) => bill.status === 'paid')
+				.reduce((total, { amount }) => total + Math.abs(amount), 0),
+		),
+	}
+	const upcomingBills = {
+		count: recurringBills.filter((bill) => bill.status === 'upcoming').length,
+		total: formatCurrency(
+			recurringBills
+				.filter((bill) => bill.status === 'upcoming')
+				.reduce((total, { amount }) => total + Math.abs(amount), 0),
+		),
+	}
+	const soonBills = {
+		count: recurringBills.filter((bill) => bill.status === 'soon').length,
+		total: formatCurrency(
+			recurringBills
+				.filter((bill) => bill.status === 'soon')
+				.reduce((total, { amount }) => total + Math.abs(amount), 0),
+		),
+	}
 
-	return json({ recurringBills: formattedRecurringBills, totalBills })
+	return json({
+		recurringBills: formattedRecurringBills,
+		totalBills,
+		paidBills,
+		upcomingBills,
+		soonBills,
+	})
 }
 
 export default function Overview() {
-	const { recurringBills, totalBills } = useLoaderData<typeof loader>()
+	const { recurringBills, totalBills, paidBills, upcomingBills, soonBills } =
+		useLoaderData<typeof loader>()
 	return (
-		<>
+		<div className="flex flex-col gap-6 @container">
 			<h1 className="text-3xl font-bold leading-tight">Recurring Bills</h1>
-			<Card
-				theme="dark"
-				className="flex items-center gap-5 sm:flex-col sm:items-start sm:justify-between sm:gap-8"
-			>
-				<Icon name="RecurringBillsOutline" className="size-10" />
-				<div className="flex flex-col gap-3" data-testid="total-bills">
-					<h2 id="total-bills" className="text-sm leading-normal">
-						Total Bills
-					</h2>
-					<p className="text-3xl font-bold leading-tight">{totalBills}</p>
+			<div className="flex flex-col gap-6 @5xl:flex-row @5xl:items-start">
+				<div className="flex flex-col gap-3 @[40rem]:flex-row @[40rem]:gap-6 @5xl:flex-col">
+					<Card
+						theme="dark"
+						className="flex flex-1 items-center gap-5 @[40rem]:flex-col @[40rem]:items-start @[40rem]:justify-between @[40rem]:gap-8"
+					>
+						<Icon name="RecurringBillsOutline" className="size-10" />
+						<div className="flex flex-col gap-3" data-testid="total-bills">
+							<h2 id="total-bills" className="text-sm leading-normal">
+								Total Bills
+							</h2>
+							<p className="text-3xl font-bold leading-tight">{totalBills}</p>
+						</div>
+					</Card>
+					<Card theme="light" className="flex-1">
+						<h2 className="mb-5 font-bold">Summary</h2>
+						<dl>
+							<SummaryItem className="border-b border-gray-100 py-4 first:pt-0 last:border-b-0 last:pb-0">
+								<SummaryItem.Title>Total Paid</SummaryItem.Title>
+								<SummaryItem.Amount
+									count={paidBills.count}
+									total={paidBills.total}
+								/>
+							</SummaryItem>
+							<SummaryItem className="border-b border-gray-100 py-4 first:pt-0 last:border-b-0 last:pb-0">
+								<SummaryItem.Title>Total Upcoming</SummaryItem.Title>
+								<SummaryItem.Amount
+									count={upcomingBills.count}
+									total={upcomingBills.total}
+								/>
+							</SummaryItem>
+							<SummaryItem
+								color="red"
+								className="border-b border-gray-100 py-4 first:pt-0 last:border-b-0 last:pb-0"
+							>
+								<SummaryItem.Title>Due Soon</SummaryItem.Title>
+								<SummaryItem.Amount
+									count={soonBills.count}
+									total={soonBills.total}
+								/>
+							</SummaryItem>
+						</dl>
+					</Card>
 				</div>
-			</Card>
-			<Card theme="light">
-				<RecurringBills recurringBills={recurringBills} />
-			</Card>
-		</>
+				<Card theme="light">
+					<RecurringBills recurringBills={recurringBills} />
+				</Card>
+			</div>
+		</div>
 	)
 }
+
+const summaryItemStyles = tv({
+	slots: {
+		base: 'flex justify-between gap-16',
+		title: 'whitespace-nowrap text-xs',
+		amount: 'whitespace-nowrap text-xs font-bold',
+	},
+	variants: {
+		color: {
+			gray: {
+				title: 'text-gray-500',
+			},
+			red: {
+				title: 'text-red',
+				amount: 'text-red',
+			},
+		},
+	},
+	defaultVariants: {
+		color: 'gray',
+	},
+})
+
+const SummaryItemContext = createContext<{ color: 'gray' | 'red' } | null>(null)
+function useSummaryItemContext() {
+	const context = useContext(SummaryItemContext)
+	if (!context) {
+		throw new Error('useSummaryItemContext must be used within a SummaryItem')
+	}
+	return context
+}
+
+function SummaryItem({
+	className,
+	color = 'gray',
+	children,
+}: {
+	className?: string
+	color?: 'gray' | 'red'
+	children: ReactNode
+}) {
+	const styles = summaryItemStyles({ color })
+	return (
+		<SummaryItemContext.Provider value={{ color }}>
+			<div
+				className={styles.base({ className, color })}
+				data-testid="definitionListItem"
+			>
+				{children}
+			</div>
+		</SummaryItemContext.Provider>
+	)
+}
+
+function SummaryItemTitle({ children }: { children: ReactNode }) {
+	const { color } = useSummaryItemContext()
+	const styles = summaryItemStyles({ color })
+	return <dt className={styles.title({ color })}>{children}</dt>
+}
+function SummaryItemAmount({ count, total }: { count: number; total: string }) {
+	const { color } = useSummaryItemContext()
+	const styles = summaryItemStyles({ color })
+	return (
+		<dd className={styles.amount({ color })}>
+			{count} ({total})
+		</dd>
+	)
+}
+SummaryItem.Title = SummaryItemTitle
+SummaryItem.Amount = SummaryItemAmount
 
 async function getLatestTransactionDate(userId: string) {
 	const latestTransaction = await prisma.transaction.findFirst({
--- tests/recurring-bills.spec.ts
+++ tests/recurring-bills.spec.ts
@@ -79,20 +79,30 @@ test('Only one instance of a recurring bill is shown', async ({
 	await expect(recurringBillsPage.recurringBills()).toHaveCount(1)
 })
 
-test('calculated totals', async ({ page, signUp, login, seedDatabase }) => {
+test('calculated totals and summary', async ({
+	page,
+	signUp,
+	login,
+	seedDatabase,
+}) => {
 	const user = await signUp()
-	const billOne = makeTransaction({
+	const billPaid = makeTransaction({
 		date: new Date('2024-01-10'),
 		isRecurring: true,
 		amount: 100 * 100,
 	})
-	const billTwo = makeTransaction({
-		date: new Date('2024-01-20'),
+	const billSoon = makeTransaction({
+		date: new Date('2023-12-12'),
 		isRecurring: true,
 		amount: 200 * 100,
 	})
+	const billUpcoming = makeTransaction({
+		date: new Date('2023-12-20'),
+		isRecurring: true,
+		amount: 300 * 100,
+	})
 	await seedDatabase({
-		transactions: [billOne, billTwo],
+		transactions: [billPaid, billSoon, billUpcoming],
 		user,
 	})
 	await login(user)
@@ -101,7 +111,11 @@ test('calculated totals', async ({ page, signUp, login, seedDatabase }) => {
 
 	await recurringBillsPage.goto()
 
-	await expect(recurringBillsPage.totalBillsSection()).toContainText('$300.00')
+	await expect(recurringBillsPage.totalBillsSection()).toContainText('$600.00')
+
+	await expect(recurringBillsPage.totalPaid()).toContainText('$100.00')
+	await expect(recurringBillsPage.totalUpcoming()).toContainText('$300.00')
+	await expect(recurringBillsPage.dueSoon()).toContainText('$200.00')
 })
 
 class RecurringBillsPage {
@@ -150,5 +164,24 @@ class RecurringBillsPage {
 		return this.page
 			.getByTestId('total-bills')
 			.filter({ has: this.page.getByRole('heading', { name: /total bills/i }) })
+	}
+
+	definition(term: string) {
+		return this.page
+			.getByTestId('definitionListItem')
+			.filter({
+				has: this.page.getByRole('term').getByText(term),
+			})
+			.getByRole('definition')
+	}
+
+	totalPaid() {
+		return this.definition('Total Paid')
+	}
+	totalUpcoming() {
+		return this.definition('Total Upcoming')
+	}
+	dueSoon() {
+		return this.definition('Due Soon')
 	}
 }
