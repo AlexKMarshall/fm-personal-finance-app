4df7c6a3a7bd509f5e9c9e96a338ff46d6730dae
AlexKMarshall <alexmarshall@qogita.com>
2024-09-24 14:35:10 +0100

    refactor: extract queries to separate file

--- app/routes/_main.transactions/route.tsx
+++ app/routes/_main.transactions/route.tsx
@@ -1,29 +1,31 @@
-import { matchSorter } from 'match-sorter'
+import { Form, useLoaderData, useSubmit } from '@remix-run/react'
 import {
-	Select,
-	Button as RACButton,
-	SelectValue,
-	Popover,
 	ListBox,
 	ListBoxItem,
+	Popover,
+	Button as RACButton,
+	Select,
+	SelectValue,
 } from 'react-aria-components'
-import { Form, useLoaderData, useSubmit } from '@remix-run/react'
 
-import { Transactions } from './Transactions'
-import { Card } from '~/components/Card'
-import { prisma } from '~/db/prisma.server'
-import type { LoaderFunctionArgs } from '@remix-run/node'
-import { requireAuthCookie } from '~/auth.server'
-import { formatCurrency, formatDate } from '~/utils/format'
-import { Label } from '~/components/Label'
-import { z } from 'zod'
 import { parseWithZod } from '@conform-to/zod'
+import type { LoaderFunctionArgs } from '@remix-run/node'
 import { useRef } from 'react'
+import { z } from 'zod'
+import { requireAuthCookie } from '~/auth.server'
+import { Card } from '~/components/Card'
 import { Icon } from '~/components/Icon'
-import type { Prisma } from '@prisma/client'
 import { Input } from '~/components/Input'
+import { Label } from '~/components/Label'
 import { Pagination } from '~/components/Pagination'
-import { paginate } from '~/utils/pagination'
+import { formatCurrency, formatDate } from '~/utils/format'
+import { Transactions } from './Transactions'
+import {
+	getCategories,
+	getTransactions,
+	type SortKey,
+	sortKeys,
+} from './queries.server'
 
 export async function loader({ request }: LoaderFunctionArgs) {
 	const { userId } = await requireAuthCookie(request)
@@ -77,15 +79,6 @@ export async function loader({ request }: LoaderFunctionArgs) {
 	}
 }
 
-const sortKeys = [
-	'date:desc',
-	'date:asc',
-	'name:asc',
-	'name:desc',
-	'amount:desc',
-	'amount:asc',
-] as const
-type SortKey = (typeof sortKeys)[number]
 const sortOptions = {
 	'date:desc': 'Latest',
 	'date:asc': 'Oldest',
@@ -250,94 +243,4 @@ export default function TransactionsRoute() {
 			</Card>
 		</>
 	)
-}
-
-async function getTransactions({
-	userId,
-	category,
-	sort = 'date:desc',
-	search,
-	page,
-	size,
-}: {
-	userId: string
-	category?: string
-	sort?: SortKey
-	search?: string
-	page: number
-	size: number
-}) {
-	function getTransactionOrderBy(
-		sort: SortKey,
-	): Prisma.TransactionOrderByWithRelationInput {
-		switch (sort) {
-			case 'date:desc':
-				return { date: 'desc' }
-			case 'date:asc':
-				return { date: 'asc' }
-			case 'name:asc':
-				return { Counterparty: { name: 'asc' } }
-			case 'name:desc':
-				return { Counterparty: { name: 'desc' } }
-			case 'amount:desc':
-				return { amount: 'desc' }
-			case 'amount:asc':
-				return { amount: 'asc' }
-		}
-	}
-
-	const sortedTransactions = await prisma.transaction.findMany({
-		where: {
-			userId,
-		},
-		select: {
-			id: true,
-			Counterparty: {
-				select: {
-					name: true,
-					avatarUrl: true,
-				},
-			},
-			amount: true,
-			date: true,
-			Category: {
-				select: {
-					name: true,
-				},
-			},
-		},
-		orderBy: getTransactionOrderBy(sort),
-	})
-	const filteredTransactions = sortedTransactions.filter((transaction) =>
-		category
-			? transaction.Category.name.toLocaleLowerCase() ===
-				category.toLocaleLowerCase()
-			: true,
-	)
-
-	if (!search) {
-		return paginate(filteredTransactions, { page, size })
-	}
-
-	const searchedTransactions = matchSorter(filteredTransactions, search, {
-		keys: ['Counterparty.name'],
-		baseSort: (a, b) => (a.index < b.index ? -1 : 1),
-	})
-
-	return paginate(searchedTransactions, { page, size })
-}
-
-function getCategories({ userId }: { userId: string }) {
-	return prisma.category.findMany({
-		where: {
-			Transactions: {
-				some: {
-					userId,
-				},
-			},
-		},
-		orderBy: {
-			name: 'asc',
-		},
-	})
 }
