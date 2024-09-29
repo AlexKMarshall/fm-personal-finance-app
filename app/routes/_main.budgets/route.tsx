719b1f4551812051624ac8ba284c1cff4a5c5ef5
Alex Marshall <alexmarshall@qogita.com>
2024-09-29 14:25:04 +0100

    test: add story for budget (#99)

--- app/routes/_main.budgets/route.tsx
+++ app/routes/_main.budgets/route.tsx
@@ -1,15 +1,11 @@
-import { json, type LoaderFunctionArgs } from '@remix-run/node'
-import { Link, useLoaderData } from '@remix-run/react'
-import clsx from 'clsx'
+import { type LoaderFunctionArgs, json } from '@remix-run/node'
+import { useLoaderData } from '@remix-run/react'
+import { isSameMonth } from 'date-fns'
 import { requireAuthCookie } from '~/auth.server'
-import { Card } from '~/components/Card'
-import { Icon } from '~/components/Icon'
 import { prisma } from '~/db/prisma.server'
 import { formatCurrency, formatDate } from '~/utils/format'
-import { List } from '../_main.transactions/Transactions'
-import { Transaction } from '~/components/Transaction'
-import { isSameMonth } from 'date-fns'
 import { getLatestTransactionDate } from '../_main.recurring-bills/recurring-bills.queries'
+import { Budget } from './Budget'
 
 export async function loader({ request }: LoaderFunctionArgs) {
 	const { userId } = await requireAuthCookie(request)
@@ -48,131 +44,9 @@ export default function BudgetsRoute() {
 		<>
 			<h1 className="text-3xl font-bold leading-tight">Budgets</h1>
 			{budgets.map((budget) => (
-				<Card
-					key={budget.id}
-					theme="light"
-					className="flex flex-col gap-5"
-					data-testid="budget"
-				>
-					<div className="flex items-center gap-4">
-						<ColorIndicator color={budget.color} />
-						<h2 className="text-xl font-bold leading-tight">
-							{budget.category}
-						</h2>
-					</div>
-					<div className="flex flex-col gap-4">
-						<h3 className="text-sm text-gray-500">
-							Maximum of {budget.amount}
-						</h3>
-						<div aria-hidden className="h-8 w-full rounded bg-beige-100 p-1">
-							<div
-								style={{ width: `${budget.spentPercent}%` }}
-								className={`h-full rounded ${getBackgroundColor(budget.color)}`}
-							/>
-						</div>
-						<dl className="grid grid-cols-2 gap-4">
-							<div className="flex gap-4">
-								<span
-									aria-hidden
-									className={`h-full w-1 rounded-lg ${getBackgroundColor(budget.color)}`}
-								/>
-								<div
-									className="flex flex-col gap-1"
-									data-testid="descriptionListItem"
-								>
-									<dt className="text-xs leading-normal text-gray-500">
-										Spent
-									</dt>
-									<dd className="text-sm font-bold leading-normal">
-										{budget.spent}
-									</dd>
-								</div>
-							</div>
-							<div className="flex gap-4">
-								<span
-									aria-hidden
-									className="h-full w-1 rounded-lg bg-beige-100"
-								/>
-								<div
-									className="flex flex-col gap-1"
-									data-testid="descriptionListItem"
-								>
-									<dt className="text-xs leading-normal text-gray-500">Free</dt>
-									<dd className="text-sm font-bold leading-normal">
-										{budget.free}
-									</dd>
-								</div>
-							</div>
-						</dl>
-					</div>
-					<Card theme="neutral" className="flex flex-col gap-5">
-						<div className="flex items-center justify-between gap-4">
-							<h3 className="font-bold">Latest Spending</h3>
-							<Link
-								to={{
-									pathname: '/transactions',
-									search: `?category=${budget.category}`,
-								}}
-								className="flex items-center gap-3 text-sm text-gray-500"
-							>
-								See All
-								<Icon name="CaretRight" className="size-2" />
-							</Link>
-						</div>
-						<List
-							items={budget.recentTransactions}
-							renderItem={(transaction) => <Transaction {...transaction} />}
-						/>
-					</Card>
-				</Card>
+				<Budget {...budget} />
 			))}
 		</>
-	)
-}
-
-function getBackgroundColor(colorName: string) {
-	switch (colorName) {
-		case 'Green':
-			return 'bg-green'
-		case 'Yellow':
-			return 'bg-yellow'
-		case 'Cyan':
-			return 'bg-cyan'
-		case 'Navy':
-			return 'bg-navy'
-		case 'Red':
-			return 'bg-red'
-		case 'Purple':
-			return 'bg-purple'
-		case 'Pink':
-			return 'bg-pink'
-		case 'Turquoise':
-			return 'bg-turquoise'
-		case 'Brown':
-			return 'bg-brown'
-		case 'Magenta':
-			return 'bg-magenta'
-		case 'Blue':
-			return 'bg-blue'
-		case 'NavyGray':
-			return 'bg-navyGray'
-		case 'ArmyGreen':
-			return 'bg-armyGreen'
-		case 'Gold':
-			return 'bg-gold'
-		case 'Orange':
-			return 'bg-orange'
-		default:
-			return 'bg-gray-500'
-	}
-}
-
-function ColorIndicator({ color }: { color: string }) {
-	return (
-		<span
-			className={clsx('size-4 rounded-full', getBackgroundColor(color))}
-			aria-hidden
-		/>
 	)
 }
 
