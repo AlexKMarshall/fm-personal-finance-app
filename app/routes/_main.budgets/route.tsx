a8fa7d2e80e2e8a491d32c38b00f554510560724
AlexKMarshall <alexmarshall@qogita.com>
2024-09-29 19:56:17 +0100

    style: make donut responsive

--- app/routes/_main.budgets/route.tsx
+++ app/routes/_main.budgets/route.tsx
@@ -85,7 +85,7 @@ export default function BudgetsRoute() {
 	const outerPaths = arcs.map((arc) =>
 		arcGenerator({
 			...arc,
-			innerRadius: 79 + (120 - 80) * ((100 - arc.data.spentPercent) / 100),
+			innerRadius: 80 + (120 - 80) * ((100 - arc.data.spentPercent) / 100),
 			outerRadius: 120,
 		}),
 	)
@@ -93,10 +93,10 @@ export default function BudgetsRoute() {
 	return (
 		<>
 			<h1 className="text-3xl font-bold leading-tight">Budgets</h1>
-			<div className="relative flex flex-col gap-6 lg:flex-row lg:items-start">
+			<div className="relative flex flex-col gap-6 @5xl:flex-row @5xl:items-start">
 				<Card
 					theme="light"
-					className="top-4 flex flex-col gap-8 lg:sticky"
+					className="top-4 flex flex-col gap-8 @xl:flex-row @5xl:sticky @5xl:flex-col"
 					aria-labelledby="spending-summary"
 					role="group"
 				>
@@ -128,7 +128,7 @@ export default function BudgetsRoute() {
 								/>
 							))}
 						</svg>
-						<div className="grid place-items-center p-20">
+						<div className="grid place-items-center p-10 sm:p-20">
 							<p className="flex flex-col items-center text-center">
 								<span className="text-3xl font-bold leading-tight">
 									{totalSpent}
@@ -139,35 +139,37 @@ export default function BudgetsRoute() {
 							</p>
 						</div>
 					</div>
-					<h2
-						id="spending-summary"
-						className="mb-6 text-xl font-bold leading-tight"
-					>
-						Spending Summary
-					</h2>
-					<dl>
-						{budgets.map((budget) => (
-							<div
-								key={budget.id}
-								className="flex items-center gap-4 border-b border-gray-500/15 pb-4 pt-4 first:pt-0 last:border-b-0 last:pb-0"
-							>
-								<ColorIndicator
-									color={budget.color}
-									shape="bar"
-									className="self-stretch"
-								/>
-								<dt className="flex-1 text-sm leading-normal text-gray-500">
-									{budget.category}
-								</dt>
-								<dd className="flex items-center gap-2 text-xs leading-normal text-gray-500">
-									<span className="text-default font-bold text-gray-900">
-										{budget.spent}
-									</span>
-									of {budget.amount}
-								</dd>
-							</div>
-						))}
-					</dl>
+					<div className="flex flex-col gap-6">
+						<h2
+							id="spending-summary"
+							className="mb-6 text-xl font-bold leading-tight"
+						>
+							Spending Summary
+						</h2>
+						<dl>
+							{budgets.map((budget) => (
+								<div
+									key={budget.id}
+									className="flex items-center gap-4 border-b border-gray-500/15 pb-4 pt-4 first:pt-0 last:border-b-0 last:pb-0"
+								>
+									<ColorIndicator
+										color={budget.color}
+										shape="bar"
+										className="self-stretch"
+									/>
+									<dt className="flex-1 text-sm leading-normal text-gray-500">
+										{budget.category}
+									</dt>
+									<dd className="flex items-center gap-2 text-xs leading-normal text-gray-500">
+										<span className="text-default font-bold text-gray-900">
+											{budget.spent}
+										</span>
+										of {budget.amount}
+									</dd>
+								</div>
+							))}
+						</dl>
+					</div>
 				</Card>
 				<div className="flex flex-1 flex-col gap-6">
 					{budgets.map((budget) => (
